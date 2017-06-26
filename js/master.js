e46759fea862ab475a68aaa6e0359ad5ddaa7938
bmoney123 <rangerred101@gmail.com>
2017-06-26 16:57:25 -0700

    Fixed slide transitions

--- js/master.js
+++ js/master.js
@@ -1,18 +1,11 @@
-var showingSubPage = false;
-var showingSubPage2 = false;
-
 window.onload = function() {
   document.body.className += " loaded";
-  var goToSubPageElements = document.getElementsByClassName("goToSubPage");
-  var i;
-  for(i = 0; i < goToSubPageElements.length; i++) {
-    goToSubPageElements[i].addEventListener("click", slideLeft);
+  if(document.getElementById("goToSubPage") != null) {
+    document.getElementById("goToSubPage").addEventListener("click", slideLeft);
   }
 
-  var goToSubPageElements = document.getElementsByClassName("goToSubPageTwo");
-  var i;
-  for(i = 0; i < goToSubPageElements.length; i++) {
-    goToSubPageElements[i].addEventListener("click", slideLeftTwo);
+  if(document.getElementById("goToSubPageTwo") != null) {
+    document.getElementById("goToSubPageTwo").addEventListener("click", slideLeftTwo);
   }
 
   var goBackElements = document.getElementsByClassName("goBack");
@@ -27,25 +20,23 @@ function hasClass(element, cls) {
 }
 
 function slideLeft() {
-  document.body.className += " slideleft";
-  showingSubPage = true;
+  document.getElementById("mainSlideContent").className += " slideLeft";
+  document.getElementById("subSlideContent").className += " slideLeft";
 }
 
 function slideLeftTwo() {
-  document.body.className += " slideleftTwo";
-  showingSubPage2 = true;
+  document.getElementById("mainSlideContent").className += " slideLeftTwo";
+  document.getElementById("subSlideContentTwo").className += " slideLeftTwo";
 }
 
 function slideRight() {
   var allElements = document.getElementsByTagName("*");
   var i;
   for(i = 0; i < allElements.length; i++) {
-    if(hasClass(allElements[i], "slideleft") && showingSubPage) {
-        allElements[i].classList.remove("slideleft");
-        showingSubPage = false;
-    } else if(hasClass(allElements[i], "slideleftTwo") && showingSubPage2) {
-        allElements[i].classList.remove("slideleftTwo");
-        showingSubPage2 = false;
+    if(hasClass(allElements[i], "slideLeft")) {
+        allElements[i].classList.remove("slideLeft");
+    } else if(hasClass(allElements[i], "slideLeftTwo")) {
+        allElements[i].classList.remove("slideLeftTwo");
     }
   }
 }
